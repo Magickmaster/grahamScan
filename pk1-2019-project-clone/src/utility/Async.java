7243f1cdd6c1ce82294fe1c8b3d14f405d585d1e
Jonas Sigel <jonas.sigel@uni-konstanz.de>
2020-02-04 13:58:03 +0100

    Moved everything down a folder

--- pk1-2019-project-clone/src/utility/Async.java
+++
@@ -1,9 +0,0 @@
-package utility;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-public class Async {
-	public static final ExecutorService executor = Executors.newCachedThreadPool();
-
-}
